648607c89875a0e10a476bed3e69fb4486171e55
Adrianogratani <adriano.gratani1@gmail.com>
2024-03-07 22:39:24 +0100

    refactoring code for collision detention, to create smooth interactions

--- index.js
+++ index.js
@@ -52,8 +52,12 @@ class Player{
 // populate each string of these 4 arrays with  a '-' or ' ': '-' = boundary, ' ' = space for movement;
 // you'll loop trough the boundary by assigning a .forEach to map. for each string inside the parameter, the callback will operate some sort of conditional rendering;
 
+// qui dentro metto le istanze di Boundary. usando forEach e uno switch statement su map in base al caso specifico '-' o ''
+// questo array viene popolato DINAMICAMENTE tramite .push() di istanze Boundary grazie al .forEach() su symbol dentro il .forEach() di map.
+// per poterle visualizzare, poi devi fare draw() di ogni istanza dentro boundaries. usa un forEach() dentro animate() per garantire il rerender frame by frame
+const boundaries = [];
 
-const boundaries = [];                  // questo array viene popolato DINAMICAMENTE tramite .push() di istanze Boundary grazie al .forEach() su symbol dentro il .forEach() di map.
+
 // le istanze dei giocatori:
 const player = new Player({          // tra parentesi metti i parametri da mandare al constructor per inizializzare 'this'
     position: {
@@ -64,7 +68,8 @@ const player = new Player({          // tra parentesi metti i parametri da manda
         x: 0,
         y: 0
     }
-})                                 // per poter visualizzare la istanza devi fare la call di draw() che sta nella classe;
+})
+// per poter visualizzare le istanze devi fare la call di draw() che sta nella classe;
 
 const keys = {
     w : {pressed : false},
@@ -75,19 +80,19 @@ const keys = {
 
 let lastKey = ''
 
-const map = [// i    i    i    i     //.forEach(row, i)
-    ['-', '-', '-', '-', '-', '-',], // j = 0              // treat each element of the array as a 'row' in the parameter of the map.forEach()
-    ['-', ' ', ' ', ' ', ' ', '-',], // j = 1              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
-    ['-', ' ', '-', '-', ' ', '-',], // j = 2              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
-    ['-', ' ', ' ', ' ', ' ', '-',], // j = 3              // nel nested .forEach() usa uno switch su (symbol): case '-' boundary.draw() mentre case ' ' non fare nulla;
-    ['-', '-', '-', '-', '-', '-',]  // j = 4              // se '-', metti una istanza DENTRO L'ARRAY boundaries.
+const map = [// i    i    i    i     //.forEach(row, i)    row e' l'array current,  j e' l'index di array in cui si trova attualmente: mi serve per creare la y di height per il fillRect nel draw() di boundary. i invece lo uso per la x ovvero width
+    ['-', '-', '-', '-', '-', '-', '-',], // j = 0              // treat each element of the array as a 'row' in the parameter of the map.forEach()
+    ['-', ' ', ' ', ' ', ' ', ' ', '-',], // j = 1              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
+    ['-', ' ', '-', ' ', '-', ' ', '-',], // j = 2              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
+    ['-', ' ', ' ', ' ', ' ', ' ', '-',], // j = 3              // nel nested .forEach() usa uno switch su (symbol): case '-' boundary.draw() mentre case ' ' non fare nulla;
+    ['-', '-', '-', '-', '-', '-', '-',]  // j = 4              // se '-', metti una istanza DENTRO L'ARRAY boundaries.
 ];
 map.forEach((row, j) =>  // il parametro j applicato ad x === ai cols di maps. x: 40 * j;
     {
         row.forEach((symbol, i) => {    // per evitare la sobrapposizione delle istanze nella stessa position, ti serve un secondo parametro i
             // console.log(symbol, i, row);
-            
-            switch(symbol) {
+            // ORA METTO LE ISTANZE:
+            switch(symbol) {    // come iterator uso symbol, i mi serve solo in quanto integer da moltiplicare per assegnare la posizione al constructor per finalizzare fillRect di draw() di Boundary
                 case '-':
                     boundaries.push(    // instead of STATIC values SUCH AS `40` for x and y, reference a variable as `static` from the class.
                         new Boundary({
@@ -108,38 +113,65 @@ map.forEach((row, j) =>  // il parametro j applicato ad x === ai cols di maps. x
      }
 )
 
+function circleCollidesWithRectangle({circle, rectangle}){
+    return(
+            circle.position.y - circle.radius + circle.velocity.y <= rectangle.position.y + rectangle.height &&
+            circle.position.x + circle.radius + circle.velocity.x >= rectangle.position.x &&
+            circle.position.y + circle.radius + circle.velocity.y >= rectangle.position.y &&
+            circle.position.x - circle.radius + circle.velocity.x <= rectangle.position.x + rectangle.width
+       )
+        }
+
+
 /// animate () riaggiorna draw() di player e il draw() e i velocity di player() ovvero player.update() in questo infiniteloop. se non fai la call di animate() non parte niente;
 function animate() {                    // aggiorni di continuo la mappa ridisegnandola in base a boundaries. inoltre, riaggiorni il player position con update
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
-    boundaries.forEach((boundary) => {    // fai il rendering di ogni blocco istanza di newBoundary creato col for each su map
-    boundary.draw();
-    })
-    
-    player.update();
-    player.velocity.y = 0
-    player.velocity.x = 0
 
-    if(keys.w.pressed && lastKey === 'w') {
-        player.velocity.y = -5
-    } else if (keys.a.pressed && lastKey === 'a') {
+    if (keys.w.pressed && lastKey === 'w') {
+     boundaries.forEach((boundary) => {
+            if (circleCollidesWithRectangle({circle: player , rectangle: boundary}) ) {
+                player.velocity.y = 0;
+            } else {
+                player.velocity.y = -5;
+            }
+     })
+
+    }  else if (keys.a.pressed && lastKey === 'a') {
         player.velocity.x = -5
+        console.log(player.velocity.x + player.position.x)
     }
     else if (keys.s.pressed && lastKey === 's') {
         player.velocity.y = 5
+        console.log(player.velocity.y + player.position.y)
     }
     else if (keys.d.pressed && lastKey === 'd') {
         player.velocity.x = 5
+        console.log(player.velocity.x + player.position.x)
     }
 
+    boundaries.forEach((boundary) => {    // fai il rendering di ogni blocco istanza di newBoundary creato col for each su map
+        boundary.draw();
 
+        // conditionals to avoid pacman from stepping through boundaries;
+        if (
+            circleCollidesWithRectangle({circle: player , rectangle: boundary})
+            ) {
+                    console.log("We are colliding!!!");
+                    player.velocity.y = 0
+                    player.velocity.x = 0
+                }
+        }
+    )
+    
+    player.update();
+    // player.velocity.y = 0
+    // player.velocity.x = 0
 
 
 }
 
-
 animate()
-
 
 addEventListener('keydown', ({ key }) => {
 switch(key){
@@ -161,8 +193,8 @@ switch(key){
         break;
 }
 
-console.log(keys.d.pressed)
-console.log(keys.s.pressed)
+// console.log(keys.d.pressed)
+// console.log(keys.s.pressed)
 
   }
 )
@@ -183,7 +215,7 @@ addEventListener('keyup', ({ key }) => {
             break;
     }
 
-    console.log(keys.d.pressed)
-    console.log(keys.s.pressed)
+    // console.log(keys.d.pressed)
+    // console.log(keys.s.pressed)
   }
 )
