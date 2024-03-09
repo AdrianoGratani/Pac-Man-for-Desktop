2dadad9b544cb880f0717d75725173938f39ddaf
Adrianogratani <adriano.gratani1@gmail.com>
2024-03-09 16:37:50 +0100

    finished map with images / map array of symbs - put inside boundaries using forEAch. generating instances for each symbol using the image object - image has a property image, its value is a call to the createImage function, createImage takes a url image as parameter and returns the image in the page

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ class Boundary {
     static width = 40;     // x   reference this static in your code below using the dot notation (class === obj sintax);
     static height = 40;    // y   reference this static in your code below using the dot notation (class === obj sintax);
 
-    constructor({position}) {   // each boundary has different properies so you need different instances
+    constructor({position, image}) {   // each boundary has different properies so you need different instances
         this.position = position;
         this.width = 40;
         this.height = 40;
+        this.image = image;
     }
 
-    draw() {  // 'disegni l'istanza chiamando il context
-        c.fillStyle = 'blue';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height) // [devi creare una istanza per poter vedere il boundary nello schermo e fare una call di draw() SULLA istanza]
+    draw() {
+       c.drawImage(this.image, this.position.x , this.position.y)
+   
     }
 }
 
-// class for the players: pacman and its enemies: both are round and share the same size, so one class for many istancies;
 
 class Player{
     constructor({position, velocity}){
@@ -33,33 +33,24 @@ class Player{
 
     draw(){
         c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)     // rules to draw the circle shape;
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
         c.fillStyle = 'yellow';
         c.fill();
         c.closePath();
-    }    // this function has to be assigned to the class === the functionality for the instances to be displayed in the game has to be inherent to their respective class;
+    }
     
-    update() {                  // update deve andare in un infinite loop per riaggiornare la pagina di continuo , ovvero animate(), insieme all'animazione del boundary;
-        this.draw();   // lo ridisegni di bolta in volta ...
-        this.position.x += this.velocity.x          // nella posizione aggiornata dalla sua velocita in direzione x o y
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
     }
 }
 
-// LO SPAZIO DEL GIOCO == LA MAPPA
-// boundaries most efficient method: create an array made of string arrays. each string array within the array has six strings
-// and you have four of these arrays. so you are kinda replicating a perimeter with a 6 cols * 4 rows.
-// populate each string of these 4 arrays with  a '-' or ' ': '-' = boundary, ' ' = space for movement;
-// you'll loop trough the boundary by assigning a .forEach to map. for each string inside the parameter, the callback will operate some sort of conditional rendering;
-
-// qui dentro metto le istanze di Boundary. usando forEach e uno switch statement su map in base al caso specifico '-' o ''
-// questo array viene popolato DINAMICAMENTE tramite .push() di istanze Boundary grazie al .forEach() su symbol dentro il .forEach() di map.
-// per poterle visualizzare, poi devi fare draw() di ogni istanza dentro boundaries. usa un forEach() dentro animate() per garantire il rerender frame by frame
 const boundaries = [];
 
 
-// le istanze dei giocatori:
-const player = new Player({          // tra parentesi metti i parametri da mandare al constructor per inizializzare 'this'
+
+const player = new Player({
     position: {
         x: Boundary.width + Boundary.width / 2,
         y: Boundary.height + Boundary.height / 2,
@@ -69,7 +60,7 @@ const player = new Player({          // tra parentesi metti i parametri da manda
         y: 0
     }
 })
-// per poter visualizzare le istanze devi fare la call di draw() che sta nella classe;
+
 
 const keys = {
     w : {pressed : false},
@@ -80,34 +71,231 @@ const keys = {
 
 let lastKey = ''
 
-const map = [// i    i    i    i     //.forEach(row, i)    row e' l'array current,  j e' l'index di array in cui si trova attualmente: mi serve per creare la y di height per il fillRect nel draw() di boundary. i invece lo uso per la x ovvero width
-    ['-', '-', '-', '-', '-', '-', '-',], // j = 0              // treat each element of the array as a 'row' in the parameter of the map.forEach()
-    ['-', ' ', ' ', ' ', ' ', ' ', '-',], // j = 1              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
-    ['-', ' ', '-', ' ', '-', ' ', '-',], // j = 2              // inspect each string within each 'row' by assigning a nested .forEach() to row within the 'row' scope.
-    ['-', ' ', ' ', ' ', ' ', ' ', '-',],
-    ['-', ' ', '-', ' ', '-', ' ', '-',],
-    ['-', ' ', ' ', ' ', ' ', ' ', '-',],
-    ['-', '-', '-', '-', '-', '-', '-',]     // j = 4              // se '-', metti una istanza DENTRO L'ARRAY boundaries.
+const map = [
+    [ '1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2' ],
+    [ '|', '.', '.', '.', '.', '.', '.', '.', '.', '.', '|' ],
+    [ '|', '.', 'B', '.', '[', '7', ']', '.', 'B', '.', '|' ],
+    [ '|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|' ],
+    [ '|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|' ],
+    [ '|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|' ],
+    [ '|', '.', 'B', '.', '[', '+', ']', '.', 'B', '.', '|' ],
+    [ '|', '.', '.', '.', '.', '_', '.', '.', '.', '.', '|' ],
+    [ '|', '.', '[', ']', '.', '.', '.', '[', ']', '.', '|' ],
+    [ '|', '.', '.', '.', '.', '^', '.', '.', '.', '.', '|' ],
+    [ '|', '.', 'B', '.', '[', '5', ']', '.', 'B', '.', '|' ],
+    [ '|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|' ],
+    [ '4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3' ]
 ];
-map.forEach((row, j) =>  // il parametro j applicato ad x === ai cols di maps. x: 40 * j;
+
+
+
+
+function createImage(src){
+    const image = new Image()
+    image.src = src;
+    return image;
+}
+
+map.forEach((row, j) =>
     {
-        row.forEach((symbol, i) => {    // per evitare la sobrapposizione delle istanze nella stessa position, ti serve un secondo parametro i
-            // console.log(symbol, i, row);
-            // ORA METTO LE ISTANZE:
-            switch(symbol) {    // come iterator uso symbol, i mi serve solo in quanto integer da moltiplicare per assegnare la posizione al constructor per finalizzare fillRect di draw() di Boundary
+        row.forEach((symbol, i) => {
+            switch(symbol) {
                 case '-':
-                    boundaries.push(    // instead of STATIC values SUCH AS `40` for x and y, reference a variable as `static` from the class.
+                    boundaries.push(
                         new Boundary({
-                            position: {   // se lascio x ed y a 0 non ho un rendering dinamico === tutte le istanze di boundary dentro boundaries, create col for each di map, finiranno tutte nello stesso punto;
-                                x: Boundary.width * i,   // reference your static property within the Boundary class === clear and easier to manage code;
-                                y: Boundary.height * j,  // === display each row // se i = 3 = 120 px SOTTO rispetto al primo
-                            }
+                            position: {
+                                x: Boundary.width * i,
+                                y: Boundary.height * j,
+                            },
+                            image: createImage('./img/pipeHorizontal.png')
+                            
                         })
                         );
-                    // console.log(boundaries) // per vedere come boundaries si popola ad ogni forEach su symbol;
-                    // console.log(boundaries[boundaries.length - 2])    // vedi ogni singolo elemento. osserva come la position cambia
                     break;
-                case ' ':
+                    
+                    case '|':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeVertical.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '1':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeCorner1.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '2':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeCorner2.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '3':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeCorner3.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '4':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeCorner4.png')
+                                
+                            })
+                            );
+                        break;
+
+                        case '7':
+                            boundaries.push(
+                                new Boundary({
+                                    position: {
+                                        x: Boundary.width * i,
+                                        y: Boundary.height * j,
+                                    },
+                                    image: createImage('./img/pipeConnectorBottom.png')
+                                    
+                                })
+                                );
+                            break;
+    
+                        case '5':
+                            boundaries.push(
+                                new Boundary({
+                                    position: {
+                                        x: Boundary.width * i,
+                                        y: Boundary.height * j,
+                                    },
+                                    image: createImage('./img/pipeConnectorTop.png')
+                                    
+                                })
+                                );
+                            break;
+    
+                    case '6':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeConnectorRight.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case 'B':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/block.png')
+                                
+                            })
+                            );
+                            break;
+                            
+                            case '^':
+                                boundaries.push(
+                                    new Boundary({
+                                        position: {
+                                            x: Boundary.width * i,
+                                            y: Boundary.height * j,
+                                        },
+                                        image: createImage('./img/capTop.png')
+                                        
+                                    })
+                                    );
+                                break;
+                                
+                    case '_':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/capBottom.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '[':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/capLeft.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case ']':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/capRight.png')
+                                
+                            })
+                            );
+                        break;
+
+                    case '+':
+                        boundaries.push(
+                            new Boundary({
+                                position: {
+                                    x: Boundary.width * i,
+                                    y: Boundary.height * j,
+                                },
+                                image: createImage('./img/pipeCross.png')
+                                
+                            })
+                            );
+                        break;
+
+                case '.':
                     break;
             }
         }
@@ -124,9 +312,7 @@ function circleCollidesWithRectangle({circle, rectangle}){
        )
         }
 
-
-/// animate () riaggiorna draw() di player e il draw() e i velocity di player() ovvero player.update() in questo infiniteloop. se non fai la call di animate() non parte niente;
-function animate() {                    // aggiorni di continuo la mappa ridisegnandola in base a boundaries. inoltre, riaggiorni il player position con update
+function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
 
@@ -217,14 +403,12 @@ function animate() {                    // aggiorni di continuo la mappa ridiseg
     }
     }
 
-    boundaries.forEach((boundary) => {    // fai il rendering di ogni blocco istanza di newBoundary creato col for each su map
+    boundaries.forEach((boundary) => {
         boundary.draw();
 
-        // conditionals to avoid pacman from stepping through boundaries;
         if (
             circleCollidesWithRectangle({circle: player , rectangle: boundary})
             ) {
-                    console.log("We are colliding!!!");
                     player.velocity.y = 0
                     player.velocity.x = 0
                 }
@@ -232,10 +416,6 @@ function animate() {                    // aggiorni di continuo la mappa ridiseg
     )
     
     player.update();
-    // player.velocity.y = 0
-    // player.velocity.x = 0
-
-
 }
 
 animate()
@@ -259,10 +439,6 @@ switch(key){
         lastKey = 'd'
         break;
 }
-
-// console.log(keys.d.pressed)
-// console.log(keys.s.pressed)
-
   }
 )
 
@@ -281,8 +457,5 @@ addEventListener('keyup', ({ key }) => {
             keys.d.pressed = false;
             break;
     }
-
-    // console.log(keys.d.pressed)
-    // console.log(keys.s.pressed)
   }
 )
